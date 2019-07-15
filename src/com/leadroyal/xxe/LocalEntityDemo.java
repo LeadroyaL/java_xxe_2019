c1c6c7f5e01b0928689a8428f982bfd7aaf4e10c
LeadroyaL <243850435@qq.com>
2019-07-15 19:54:59 +0800

    Add LocalEntityDemo.

---
+++ src/com/leadroyal/xxe/LocalEntityDemo.java
@@ -0,0 +1,26 @@
+package com.leadroyal.xxe;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class LocalEntityDemo {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = dbf.newDocumentBuilder();
+        Document doc = builder.parse(new File("local.xml"));
+        NodeList nodes = doc.getChildNodes();
+        for (int i = 0; i < nodes.getLength(); i++) {
+            if (nodes.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                System.out.println(nodes.item(i).getTextContent());
+            }
+        }
+    }
+}
